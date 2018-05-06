aef2f3f071644bc7fd622ed3e617a49a6146fa25
Serge <vspox@yandex.ru>
2018-05-06 20:12:56 +0300

    working movies

--- dist/js/app.js
+++ dist/js/app.js
@@ -44,7 +44,7 @@ var handleMovieList = angular.module("handleMovieList",["ngStorage"]);
     });
 */
 
-var handleMovieList = angular.module("handleMovieList", ['ngStorage']);
+/*var handleMovieList = angular.module("handleMovieList", ['ngStorage']);
 handleMovieList.controller("addMovie", function($scope, $localStorage) {
 
     $scope.templ = []; // устанавливаем значение по умолчанию. Если в localStorage нету companies, он создаться пустым массивом
@@ -72,4 +72,43 @@ handleMovieList.controller("addMovie", function($scope, $localStorage) {
         $scope.actors = "";
 
     };
+});*/
+var handleMovieList = angular.module("handleMovieList", ['ngStorage']);
+handleMovieList.controller("addMovie", function($scope, $localStorage) {
+
+    $localStorage.$default({
+      companies: [] // устанавливаем значение по умолчанию. Если в localStorage нету companies, он создаться пустым массивом
+    });
+
+    $scope.companies = [];
+    $scope.companies = $localStorage.companies; // присваиваем к $scope наш массив. Он или пустой или с данными.
+    $scope.addRow = function() {
+
+        /*if ($scope.companies !==undefined) {*/
+        $scope.companies.push({
+            'title': $scope.title,
+            'year': $scope.year,
+            'country': $scope.country,
+            'genre': $scope.genre,
+            'actors': $scope.actors,
+            'description': $scope.description,
+            'poster': $scope.poster
+        }); // когда обновляется содержмиое массива, будет перезаписываться значение в localStorage
+        $scope.title = '';
+        $scope.year = '';
+        $scope.country = '';
+        $scope.genre = '';
+        $scope.actors = '';
+        $scope.description = '';
+        $scope.poster = '';
+        /*} else {
+            console.log ("gaga");
+        }*/
+    };
+    $scope.deleteMovie = function (item) {
+        var data = $scope.companies.indexOf(item);
+        $scope.companies.splice(data, 1);
+
+        //console.log(deleteMovie());
+    }
 });
